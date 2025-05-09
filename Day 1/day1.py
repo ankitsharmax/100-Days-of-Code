acc1bc75448a675efd303604fc44b9a77f864acb
ankitsharmax <30682419+ankitsharmax@users.noreply.github.com>
2025-05-09 12:05:02 +0530

    Day1

---
+++ Day 1/day1.py
@@ -0,0 +1,7 @@
+welcome_message = """
+Welcom to the Band Name Generator
+What's the name of the city you grew up in?
+"""
+city_name = input(welcome_message)
+pet_name = input("What's your pet's name?\n")
+print(f"Your band name could be {city_name} {pet_name}")
